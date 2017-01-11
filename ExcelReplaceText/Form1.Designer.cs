a9dd29cf2ce5172195889c8c9037a3abf4dcc8df
TuanCM91 <tuancm91@gmail.com>
2017-01-11 13:37:33 +0900

    first commit

---
+++ ExcelReplaceText/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ExcelReplaceText
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnReplace = new System.Windows.Forms.Button();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Find = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Replace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnReplace
+			// 
+			this.btnReplace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnReplace.Location = new System.Drawing.Point(546, 449);
+			this.btnReplace.Name = "btnReplace";
+			this.btnReplace.Size = new System.Drawing.Size(75, 23);
+			this.btnReplace.TabIndex = 1;
+			this.btnReplace.Text = "Replace";
+			this.btnReplace.UseVisualStyleBackColor = true;
+			this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnBrowse.Location = new System.Drawing.Point(13, 449);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 2;
+			this.btnBrowse.Text = "Browse...";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Find,
+            this.Replace});
+			this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 21;
+			this.dataGridView1.Size = new System.Drawing.Size(608, 150);
+			this.dataGridView1.TabIndex = 3;
+			this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+			// 
+			// Find
+			// 
+			this.Find.HeaderText = "Find";
+			this.Find.Name = "Find";
+			this.Find.Width = 250;
+			// 
+			// Replace
+			// 
+			this.Replace.HeaderText = "Replace";
+			this.Replace.Name = "Replace";
+			this.Replace.Width = 250;
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(13, 169);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+			this.listBox1.Size = new System.Drawing.Size(608, 251);
+			this.listBox1.TabIndex = 4;
+			this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(633, 484);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.btnReplace);
+			this.MinimumSize = new System.Drawing.Size(641, 511);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnReplace;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Find;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Replace;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ ExcelReplaceText/Form1.cs
@@ -0,0 +1,214 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelReplaceText
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+
+		}
+
+		IXLWorksheet ws = null;
+		const string findString = "統一商品コード";
+		const string replaceWithString = "高山商品コード";
+
+		private void btnReplace_Click(object sender, EventArgs e)
+		{
+			if (ws == null)
+			{
+				return;
+			}
+
+			var selectedItems = listBox1.SelectedItems;
+			List<MatchValue> toBeRemoved = new List<MatchValue>();
+			IXLRange printArea = ws.PageSetup.PrintAreas.FirstOrDefault();
+			int colCount = printArea.ColumnCount();
+			foreach (MatchValue item in selectedItems)
+			{
+				string newValue = item.CellValue
+					.Remove(item.MatchIndex, item.ReplaceString.Length)
+					.Insert(item.MatchIndex, replaceWithString);
+				ws.Cell(item.CellPosition).Value = newValue;
+				ws.Cell(item.CellPosition).RichText.Substring(item.MatchIndex, replaceWithString.Length).FontColor = XLColor.Red;
+
+				ws.Cell(item.CellPosition.RowNumber, colCount + 1).Value = DateTime.Now.ToString("yyyy/MM/dd") + " 変更";
+				ws.Cell(item.CellPosition.RowNumber, colCount + 1).Style.Font.FontColor = XLColor.Red;
+				toBeRemoved.Add(item);
+			}
+
+			foreach (var item in toBeRemoved)
+			{
+				listBox1.Items.Remove(item);
+			}
+
+			string saveAsName = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + "_Edit" + Path.GetExtension(fileName));
+			ws.Workbook.SaveAs(saveAsName);
+		}
+
+		private static string fileName = null;
+
+		class MatchValue
+		{
+			public IXLAddress CellPosition { get; set; }
+			public int MatchIndex { get; set; }
+			public string CellValue { get; set; }
+			public string ReplaceString { get; set; }
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog saveFileDialog = new OpenFileDialog();
+			saveFileDialog.Filter = "Excel|*.xlsx";
+			saveFileDialog.Title = "Choose a location";
+			saveFileDialog.FileName = Path.GetFileName(fileName);
+
+			if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				fileName = saveFileDialog.FileName;
+				using (var workbook = new XLWorkbook(saveFileDialog.OpenFile(), XLEventTracking.Disabled))
+				{
+					ws = workbook.Worksheet("3章 処理説明書");
+					if (ws != null)
+					{
+						var cells = ws.CellsUsed(x => CompareValueTo(x, findString));
+						foreach (var cell in cells)
+						{
+							var indexes = AllIndexesOf(cell.Value as string, findString);
+							foreach (var index in indexes)
+							{
+								MatchValue itemValue = new MatchValue()
+								{
+									CellPosition = cell.Address,
+									MatchIndex = index,
+									CellValue = cell.Value as string,
+									ReplaceString = findString,
+								};
+
+								listBox1.Items.Add(itemValue);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private static bool CompareValueTo(IXLCell value, string compareToValue)
+		{
+			if (value == null || compareToValue == null)
+			{
+				return false;
+			}
+
+			string stringValue = value.Value as string;
+			if (stringValue == null)
+			{
+				return false;
+			}
+
+			if (stringValue.IndexOf(compareToValue, StringComparison.OrdinalIgnoreCase) != -1)
+			{
+				return true;
+			}
+
+			return value.Equals(compareToValue);
+		}
+
+		public static List<int> AllIndexesOf(string str, string value)
+		{
+			if (String.IsNullOrEmpty(value))
+				throw new ArgumentException("the string to find may not be empty", "value");
+			List<int> indexes = new List<int>();
+			for (int index = 0; ; index += value.Length)
+			{
+				index = str.IndexOf(value, index);
+				if (index == -1)
+					return indexes;
+				indexes.Add(index);
+			}
+		}
+
+		private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+		{
+			e.DrawBackground();
+
+			MatchValue value = (MatchValue)listBox1.Items[e.Index];
+			int length = 0;
+			Point pt = new Point(e.Bounds.X, e.Bounds.Y);
+			if (value.MatchIndex != 0)
+			{
+				string beforeString = new string(value.CellValue.Take(value.MatchIndex).ToArray());
+				TextRenderer.DrawText(e.Graphics, beforeString, Font, pt, Color.Black);
+				pt.X += TextRenderer.MeasureText(beforeString, Font).Width - 5;
+				length += beforeString.Length;
+			}
+
+			var boldFont = new Font(Font.FontFamily, Font.Size, FontStyle.Bold);
+			TextRenderer.DrawText(e.Graphics, value.ReplaceString, boldFont, pt, Color.Black);
+			pt.X += TextRenderer.MeasureText(value.ReplaceString, Font).Width + 3;
+
+			length += value.ReplaceString.Length;
+			if (length < value.CellValue.Length)
+			{
+				string afterString = new string(value.CellValue.Skip(length).ToArray());
+				TextRenderer.DrawText(e.Graphics, afterString, Font, pt, Color.Black);
+			}
+
+			e.DrawFocusRectangle();
+		}
+
+		private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Control && e.KeyCode == Keys.V)
+			{
+				DataObject o = (DataObject)Clipboard.GetDataObject();
+				if (o.GetDataPresent(DataFormats.Text))
+				{
+					if (dataGridView1.RowCount > 0)
+						dataGridView1.Rows.Clear();
+
+					if (dataGridView1.ColumnCount > 0)
+						dataGridView1.Columns.Clear();
+
+					bool columnsAdded = false;
+					string[] pastedRows = Regex.Split(o.GetData(DataFormats.Text).ToString().TrimEnd("\r\n".ToCharArray()), "\r\n");
+					foreach (string pastedRow in pastedRows)
+					{
+						string[] pastedRowCells = pastedRow.Split(new char[] { '\t' });
+
+						if (!columnsAdded)
+						{
+							for (int i = 0; i < pastedRowCells.Length; i++)
+								dataGridView1.Columns.Add("col" + i, pastedRowCells[i]);
+
+							columnsAdded = true;
+							continue;
+						}
+
+						dataGridView1.Rows.Add();
+						int myRowIndex = dataGridView1.Rows.Count - 1;
+
+						using (DataGridViewRow dataGridView1Row = dataGridView1.Rows[myRowIndex])
+						{
+							for (int i = 0; i < pastedRowCells.Length; i++)
+								dataGridView1Row.Cells[i].Value = pastedRowCells[i];
+						}
+					}
+				}
+			}
+		}
+	}
+}
